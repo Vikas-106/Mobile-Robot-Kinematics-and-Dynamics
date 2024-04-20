1913ef983cb6790005a605fab5a3eb763fd21954
Vikas-106 <iamvikas558@gmail.com>
2024-04-20 22:53:48 +0530

    Added code for mecanum 3 wheeled omega velocity

--- Two_Wheeled_mobile_model.m
+++ Two_Wheeled_mobile_model.m
@@ -13,7 +13,7 @@ t = 0:dt:ts; % Time span of the simulation
 
 x0 = 1; % x-coordinate
 y0 = 1; % y-coordinate
-psi0 = pi/3; % Angle of rotation 
+psi0 = 0; % Angle of rotation 
 
 eta0 = [x0;y0;psi0];
 
@@ -31,15 +31,30 @@ for i = 1:length(t)
     j_psi = [cos(psi), -sin(psi) , 0 ;  
              sin(psi), cos(psi) , 0 ;
              0, 0 , 1];
+
     %% input velocity for both the wheels
 
-    omega_1 = -0.5; % Angular velocity of wheel 1 (left)
+    omega_1 = 0.5; % Angular velocity of wheel 1 (left)
     omega_2 = 0.5; % Angular velocity of wheel 2  (right)
 
     omega = [omega_1 ;omega_2];
     W = [a/2 ,a/2 ;        
          0  ,0 ;               
          -a/(2*d) ,a/(2*d)];
+
+    %% input velocity for macanum wheel (3 wheels with equal space )
+
+    % omega_1 = 0; 
+    % omega_2 = 0.5; 
+    % omega_3 = -0.5;
+    % 
+    % omega = [omega_1 ;omega_2 ; omega_3];
+    % 
+    % W = (a/3)*[0 -sqrt(3) sqrt(3) ;
+    %            2 -1 -1 ;
+    %            1/d 1/d 1/d];
+
+    %----------------------------------------------------
 
     zeta(:,i) = W*omega;
 
